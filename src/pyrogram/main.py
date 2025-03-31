TelegramPlayground/bmt

commit 4f2d8f5c6205209daf22a1fea6d22a4f4c50076e

Update Hydrogram results

src/pyrogram/main.py
@@ -3,8 +3,8 @@ import logging
 import os
 from datetime import datetime
 from json import dumps
-from pyrogram import Client, __version__
-from pyrogram.raw.all import layer
+from hydrogram import Client, __version__
+from hydrogram.raw.all import layer
 
 
 logging.basicConfig(

commit 620d53e8cff5cf4e67ff824161959363da7df114

(fix): Pyrogram import
Regression was introduced in 4f2d8f5

.github/workflows/hydrogram.yml
@@ -32,6 +32,7 @@ jobs:
           pip install --no-cache-dir --force-reinstall --upgrade Hydrogram TgCrypto==1.2.5 > /dev/null 2>&1
           sed -i s/pyrogram/hydrogram/g src/pyrogram/main.py
           P=$(python src/pyrogram/main.py)
+          sed -i s/hydrogram/pyrogram/g src/pyrogram/main.py
           pip uninstall -y hydrogram tgcrypto > /dev/null 2>&1
           echo "${P}" > out/hydrogram.json
 

src/pyrogram/main.py
@@ -3,8 +3,8 @@ import logging
 import os
 from datetime import datetime
 from json import dumps
-from hydrogram import Client, __version__
-from hydrogram.raw.all import layer
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
 
 
 logging.basicConfig(