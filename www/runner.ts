TelegramPlayground/bmt

commit 71ad5a4c232451929069cbd1b5933690e8ee51c5

Add support for WTelegramClient

www/runner.ts
@@ -92,6 +92,12 @@ const libs: Array<Library> = [
     display: "mtcute",
     language: "typescript",
   },
+  {
+    name: "wtelegramclient",
+    repo: "https://github.com/mtcute/mtcute",
+    display: "WTelegramClient",
+    language: "C#",
+  },
 ];
 
 interface ActionData {

.github/workflows/wtelegramclient.yml
@@ -1,0 +1,38 @@
+name: wtelegramclient
+
+on:
+  workflow_dispatch:
+  schedule:
+    - cron: "3 2 * * 4"
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Checkout repository contents
+        uses: actions/checkout@v4
+
+      - name: Setup dotnet
+        uses: actions/setup-dotnet@v4
+        with:
+          dotnet-version: "8.x"
+
+      - name: Run script
+        env:
+          API_ID: ${{ secrets.APP_ID }}
+          API_HASH: ${{ secrets.API_HASH }}
+          BOT_TOKEN: ${{ secrets.BOT_TOKEN }}
+          # TG_SESSION: ${{ secrets.TG_SESSION }}
+          # FLOOD_WAIT_SLEEP_TIME: ${{ secrets.FLOOD_WAIT_SLEEP_TIME }}
+          MESSAGE_LINK: ${{ secrets.MESSAGE_LINK }}
+        run: |
+          echo "Beginning WTelegramClient test"
+          mkdir -p out
+          cd src/wtelegramclient
+          dotnet run src/wtelegramclient
+          cd ../..
+
+      - name: Commit and push changes
+        run: |
+          LIBRARY="WTelegramClient" ./.github/_commit.sh

.gitignore
@@ -354,3 +354,12 @@ dist
 .yarn/build-state.yml
 .yarn/install-state.gz
 .pnp.*
+
+# Visual Studio cache/options directory
+.vs/
+
+# Build results
+[Dd]ebug/
+[Rr]elease/
+bin/
+obj/

src/wtelegramclient/bmt.csproj
@@ -1,0 +1,14 @@
+﻿<Project Sdk="Microsoft.NET.Sdk">
+
+  <PropertyGroup>
+    <OutputType>Exe</OutputType>
+    <TargetFramework>net8.0</TargetFramework>
+    <ImplicitUsings>enable</ImplicitUsings>
+    <Nullable>enable</Nullable>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <PackageReference Include="WTelegramClient" Version="*" />
+  </ItemGroup>
+
+</Project>

src/wtelegramclient/main.cs
@@ -1,0 +1,46 @@
+using System.Reflection;
+using System.Text.Json;
+using TL;
+
+// NOTE: For reliable measurements, the session file WTelegram.session should be conserved between runs.
+
+using var client = new WTelegram.Client(Environment.GetEnvironmentVariable);
+await client.LoginBotIfNeeded();
+var mcm = await client.GetMessageByLink(Environment.GetEnvironmentVariable("MESSAGE_LINK"));
+if (mcm.messages[0] is not Message { media: MessageMediaDocument { document: Document document } } msg)
+	return 1; // failed to find document to download
+WTelegram.Helpers.Log = (l, s) => { };
+var peer = mcm.UserOrChat(msg.peer_id).ToInputPeer();
+var ms = new MemoryStream((int)document.size);
+
+var dstart = DateTimeOffset.UtcNow;
+await client.DownloadFileAsync(document, ms);
+var dend = DateTimeOffset.UtcNow;
+
+ms.Position = 0;
+
+var ustart = DateTimeOffset.UtcNow;
+var uploadedFile = await client.UploadFileAsync(ms, "WTelegramClient.bin");
+_ = client.SendMediaAsync(peer, "", uploadedFile, reply_to_msg_id: msg.id);
+var uend = DateTimeOffset.UtcNow;
+
+var data = new
+{
+	version = typeof(WTelegram.Client).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion?.Split('+')[0],
+	layer = Layer.Version,
+	file_size = document.size,
+	download = new {
+		start_time = dstart.ToUnixTimeMilliseconds() / 1000.0,
+		end_time = dend.ToUnixTimeMilliseconds() / 1000.0,
+		time_taken = (dend - dstart).TotalSeconds,
+		avg_speed = $"{document.size / (dend - dstart).TotalSeconds / 1024 / 1024:N2} MB/s",
+	},
+	upload = new {
+		start_time = ustart.ToUnixTimeMilliseconds() / 1000.0,
+		end_time = uend.ToUnixTimeMilliseconds() / 1000.0,
+		time_taken = (uend - ustart).TotalSeconds,
+		avg_speed = $"{document.size / (uend - ustart).TotalSeconds / 1024 / 1024:N2} MB/s",
+	}
+};
+File.WriteAllText("../../out/wtelegramclient.json", JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true }));
+return 0;

commit 72e292812d3d1a0957963e5261ec92acff8ccbf7

(fix): Url for WTelegramClient library
Regression was introduced in 71ad5a4

www/runner.ts
@@ -94,7 +94,7 @@ const libs: Array<Library> = [
   },
   {
     name: "wtelegramclient",
-    repo: "https://github.com/mtcute/mtcute",
+    repo: "https://github.com/wiz0u/WTelegramClient",
     display: "WTelegramClient",
     language: "c-sharp",
   },