5701e36d457f260cf062b36b9857509118dfe983
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2025-04-11 10:38:51 +0530

    Add warning disclaimer

--- www/runner.ts
+++ www/runner.ts
@@ -297,8 +297,12 @@ function createReqdHatml(
                   </tr>
                   ${innerCards}
                 </table>
+                <p>
+                  Each library was given a 2 GB file to download, present in a active public Telegram Chat, and then re-upload to the same public chat.
+                  All benchmarks were run inside GitHub hosted-runners.
+                </p>
                 <p style="display:none;">
-                    Data updated at ${DateStamp} • <a href="https://github.com/TelegramPlayGround/bmt">View Source</a> • <a href="https://github.com/amarnathcjd">@AmarnathCJD</a>
+                    Data updated at ${DateStamp} • <a href="https://github.com/TelegramPlayGround/bmt">View Source</a>
                 </p>
             </article>
         </div>
